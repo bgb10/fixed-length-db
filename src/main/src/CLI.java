137d9aeb8aa5b57e34f629987948d3dd73ca4454
bgb10 <bgb10@naver.com>
2024-04-09 16:56:53 +0900

    feat: uses query engine when creating table in CLI

--- src/main/src/CLI.java
+++ src/main/src/CLI.java
@@ -1,16 +1,37 @@
+import javax.management.Query;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class CLI {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        QueryEngine qe = new QueryEngine();
 
         while (true) {
             System.out.println("\n1. Create Table\n2. Insert Tuple\n3. Delete Tuple\n4. Select\n5. Select All\n6. Exit");
             System.out.print("Enter your choice: ");
             int choice = scanner.nextInt();
 
+            String tableName;
+            boolean tableExists;
+
             switch (choice) {
                 case 1:
+                    System.out.print("Enter table name: ");
+                    tableName = scanner.next();
+                    System.out.print("Enter number of columns: ");
+                    int numColumns = scanner.nextInt();
+                    HashMap<String, Integer> columns = new HashMap<>();
+                    for (int i = 0; i < numColumns; i++) {
+                        System.out.print("Enter column name: ");
+                        String columnName = scanner.next();
+                        System.out.print("Enter column bytes: ");
+                        Integer length = scanner.nextInt();
+                        columns.put(columnName, length);
+                    }
+                    System.out.print("Enter PK column name: "); // Ask for primary key column name
+                    String primaryKey = scanner.next();
+                    qe.createTable(tableName, columns, primaryKey);
                     break;
                 case 2:
                     break;
