13b517abd3f330fa17e6af82cc2a172df5973ded
bgb10 <bgb10@naver.com>
2024-04-10 15:17:55 +0900

    fix: now free list node filled with NULL

--- src/main/src/FileManager.java
+++ src/main/src/FileManager.java
@@ -19,13 +19,7 @@ public class FileManager {
 //            int blockingFactor = BLOCK_SIZE / recordSize;
             byte[] freeListRecord = new byte[recordSize];
             // Next free node index (null is 0)
-            freeListRecord[0] = '!'; // ! character
-            freeListRecord[1] = '@'; // @ character
-            freeListRecord[2] = '#'; // # character
             freeListRecord[3] = '0';   // Next free node index (initially 0)
-            for (int i = 4; i < recordSize; i++) {
-                freeListRecord[i] = '#';
-            }
             file.write(freeListRecord);
             file.close();
         } catch (Exception e) {
